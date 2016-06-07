4916e40d6cfb835ad979c987cdf47e40f205ac56
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-06-07 05:20:05 -0600

    Update script

--- scripts/export_bib.py
+++ scripts/export_bib.py
@@ -16,7 +16,7 @@ def main():
     user_id = os.environ['ZOTERO_USER_ID']
     collection_key = os.environ['ZOTERO_COLLECTION_KEY']
 
-    url = 'https://api.zotero.org/users/{}/collections/{}/items?key={}&format=bibtex'.format(user_id, collection_key, api_key)
+    url = 'https://api.zotero.org/users/{}/collections/{}/items?key={}&format=bibtex&limit=1000'.format(user_id, collection_key, api_key)
     r = requests.get(url)
 
     with open('./content/blog.bib', 'w') as f:
