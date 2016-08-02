25629e7d2e6399a4472c1d2ff1d8530ab9cfb3e9
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-08-02 05:44:25 -0600

    Update convert script

--- convert-ipynb.py
+++ convert-ipynb.py
@@ -10,6 +10,8 @@ from subprocess import check_output
 from subprocess import Popen, PIPE
 import json
 
+converted = False
+
 def convert_notebook_to_html(file_name):
     out = check_output(["jupyter-nbconvert", "content/notebooks/{}".format(file_name), "--to", "html"])
 
@@ -19,11 +21,15 @@ def convert_html_to_json(file_name):
 
 def notebook_convert(key, value, format, meta):
 
+    if converted:
+        sys.stderr.write("{}, {}, {}, {}".format(key, value, format, meta))
+
     if key == 'Para' and value[0]['c'][0:2] == '{%' and value[-1]['c'][-2:] == '%}' and value[2]['c']=='notebook' :
         convert_notebook_to_html(value[4]['c'])
         convert_html_to_json(value[4]['c'].replace('.ipynb', '.html'))
         tuple_notebook = tuple(json.loads(convert_html_to_json(value[4]['c'].replace('.ipynb', '.html')))[1][0]['c']) # Remove unMeta
         sys.stderr.write("Converting notebook {}\n".format(value[4]['c']))
+        converted = True
         return Div(*tuple_notebook)
     
 if __name__ == "__main__":
