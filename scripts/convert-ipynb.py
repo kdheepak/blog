257596c135e33be40cd40ce7bb544163b3b3fb31
Dheepak Krishnamurthy <me@kdheepak.com>
2020-05-28 03:55:54 -0600

    Update convert-ipynb

--- scripts/convert-ipynb.py
+++ scripts/convert-ipynb.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 
 import os
@@ -10,33 +10,56 @@ from subprocess import check_output
 from subprocess import Popen, PIPE
 import json
 
+
 def convert_notebook_to_html(file_name):
-    out = check_output(["jupyter-nbconvert", "content/notebooks/{}".format(file_name), "--to", "html"])
+    out = check_output(
+        ["jupyter-nbconvert", "content/notebooks/{}".format(file_name), "--to", "html"]
+    )
+
 
 def convert_html_to_json(file_name):
-    out = check_output(["pandoc", "content/notebooks/{}".format(file_name), "-f", "html+tex_math_dollars+tex_math_single_backslash", "-t", "json"])
+    out = check_output(
+        [
+            "pandoc",
+            "content/notebooks/{}".format(file_name),
+            "-f",
+            "html+tex_math_dollars+tex_math_single_backslash",
+            "-t",
+            "json",
+        ]
+    )
     return out
+
 
 def remove_html(file_name):
     try:
-        os.remove('content/notebooks/{}'.format(file_name).replace('ipynb', 'html'))
+        os.remove("content/notebooks/{}".format(file_name).replace("ipynb", "html"))
     except:
         pass
     try:
-        os.remove('{}'.format(file_name).replace('ipynb', 'html'))
+        os.remove("{}".format(file_name).replace("ipynb", "html"))
     except:
         pass
 
 
 def notebook_convert(key, value, format, meta):
 
-    if key == 'Para' and value[0]['c'][0:2] == '{%' and value[-1]['c'][-2:] == '%}' and value[2]['c']=='notebook' :
-        convert_notebook_to_html(value[4]['c'])
+    if (
+        key == "Para"
+        and value[0]["c"][0:2] == "{%"
+        and value[-1]["c"][-2:] == "%}"
+        and value[2]["c"] == "notebook"
+    ):
+        convert_notebook_to_html(value[4]["c"])
         # sys.stderr.write("{}".format(type(json.loads(convert_html_to_json(value[4]['c'].replace('.ipynb', '.html')))["blocks"][0]['c'][1][0]['c'])))
-        tuple_notebook = tuple(json.loads(convert_html_to_json(value[4]['c'].replace('.ipynb', '.html')))["blocks"][0]['c'][1][0]['c']) # Remove unMeta
-        sys.stderr.write("Converting notebook {}\n".format(value[4]['c']))
+        tuple_notebook = tuple(
+            json.loads(convert_html_to_json(value[4]["c"].replace(".ipynb", ".html")))[
+                "blocks"
+            ][0]["c"][1][0]["c"]
+        )  # Remove unMeta
+        sys.stderr.write("Converting notebook {}\n".format(value[4]["c"]))
 
-        remove_html(value[4]['c'])
+        remove_html(value[4]["c"])
 
         return Div(*tuple_notebook)
 
@@ -48,7 +71,6 @@ def notebook_convert(key, value, format, meta):
         return Header(*value)
         # return []
 
+
 if __name__ == "__main__":
     toJSONFilter(notebook_convert)
-
-
