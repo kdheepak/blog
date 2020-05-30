1bdf9bd7b129b2cb2ed777154580ee9983d5133c
Dheepak Krishnamurthy <me@kdheepak.com>
2020-05-30 06:19:29 -0600

    Update website

--- website.nim
+++ website.nim
@@ -143,13 +143,11 @@ proc render(file: string): JsonNode =
   if ofilename != "index" and ofilename != "404":
     args = &"{args} -V comments"
 
-  let sidenote_filter = absolutePath(joinPath("scripts", "sidenote.lua"))
-  args = &"{args} --lua-filter={sidenote_filter} {filters}"
+  args = &"{args} --lua-filter=\"../scripts/sidenote.lua\" {filters}"
 
   let csl = absolutePath(joinPath("templates", "csl.csl"))
   if post{"csl"}.getStr == "" and fileExists(csl):
-    let ref_section_level_filter = absolutePath(joinPath("scripts", "ref-section-level.lua"))
-    args = &"{args} --csl {csl} --metadata link-citations=true --metadata notes-after-punctuation=false --metadata reference-section-title=\"References\" section-refs-level=2 --lua-filter={ref_section_level_filter}"
+    args = &"{args} --csl {csl} --metadata link-citations=true --metadata notes-after-punctuation=false --metadata reference-section-title=\"References\""
 
   let ds = post{"date"}.getStr
   if ds != "":
