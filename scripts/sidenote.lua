a45e71acaf94cec07a70af0ad3e4fe5a786d55ef
Dheepak Krishnamurthy <me@kdheepak.com>
2020-05-30 04:19:28 -0600

    Update sidenote.lua

--- scripts/sidenote.lua
+++ scripts/sidenote.lua
@@ -6,7 +6,8 @@ function Note(elem)
     -- io.stderr:write(elem.t, "\n")
     -- return pandoc.Span(elem.c[1].c, {id = '', class = 'sidenote'})
     content = {
-        pandoc.RawInline('html', '<label for="sn-' .. i .. '" class="sidenote-number margin-toggle"/>'),
+        pandoc.RawInline('html', '<label for="sn-' .. i .. '" class="sidenote-number margin-toggle">'),
+        pandoc.RawInline('html', '</label>'),
         pandoc.RawInline('html', '<input type="checkbox" id="sn-' .. i .. '" class="margin-toggle"/>'),
         pandoc.Span(elem.c[1].c, {id = '', class = 'sidenote'}),
     }
