6729f686475c69ac063a2529758da4d7a41ece50
Dheepak Krishnamurthy <me@kdheepak.com>
2020-05-30 20:28:31 -0600

    Update sidenote.lua

--- scripts/sidenote.lua
+++ scripts/sidenote.lua
@@ -1,10 +1,21 @@
 i = 1
+j = 1
 
-function Note(elem)
-    -- io.stderr:write(elem.t, "\n")
-    -- elem.t = "Span"
-    -- io.stderr:write(elem.t, "\n")
-    -- return pandoc.Span(elem.c[1].c, {id = '', class = 'sidenote'})
+function getMarginNote(elem)
+    table.remove(elem.c[1].c,1)
+    table.remove(elem.c[1].c,1)
+    table.remove(elem.c[1].c,1)
+    content = {
+        pandoc.RawInline('html', '<label for="mn-' .. j .. '" class="margin-toggle">'),
+        pandoc.RawInline('html', '</label>'),
+        pandoc.RawInline('html', '<input type="checkbox" id="mn-' .. j .. '" class="margin-toggle"/>'),
+        pandoc.Span(elem.c[1].c, {id = '', class = 'marginnote'}),
+    }
+    j = j + 1
+    return pandoc.Span(content, {})
+end
+
+function getSideNote(elem)
     content = {
         pandoc.RawInline('html', '<label for="sn-' .. i .. '" class="sidenote-number margin-toggle">'),
         pandoc.RawInline('html', '</label>'),
@@ -13,4 +24,15 @@ function Note(elem)
     }
     i = i + 1
     return pandoc.Span(content, {})
+end
+
+function Note(elem)
+    if elem.c[1].t == "Para" then
+        p = elem.c[1]
+        if p.c[1].t == "Emph" and p.c[1].c[1].t == "Str" and p.c[1].c[1].c == "aside" and p.c[2].t == "Str" and p.c[2].c == ":" and p.c[3].t == "Space" then
+            return getMarginNote(elem)
+        end
+    else
+        return getSideNote(elem)
+    end
 end
