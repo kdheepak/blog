f70a34143a6e54ec916d6a538802683cd6df5c15
Dheepak Krishnamurthy <me@kdheepak.com>
2020-06-27 03:21:35 -0600

    Update files

--- content/js/kudos.js
+++ content/js/kudos.js
@@ -1,4 +1,63 @@
 (function() {
+  // used to fade elements in and out
+  // https://stackoverflow.com/questions/30206054/vanilla-javascript-fadein-fadeout-without-jquery
+
+  // adding a _ method on which we can attach utility functions
+  function _(el) {
+    if (!(this instanceof _)) {
+      return new _(el);
+    }
+
+    this.el = typeof el === 'string' ? document.querySelectorAll(el) : el;
+  }
+
+  _.prototype._applyFunc = function(func) {
+    var slicedArgs = Array.prototype.slice.call(arguments, 1); // all arguments except the function name
+
+    if(typeof this.el.forEach === 'function') { // el is an array
+      this.el.forEach(function(el) {
+        this['_' + func].apply(this, [el].concat(slicedArgs));
+      }.bind(this));
+    } else { // el is a single item
+      this['_' + func].apply(this, [this.el].concat(slicedArgs));
+    }
+  };
+
+  _.prototype.fade = function(type, ms) {
+    this._applyFunc('fade', type, ms);
+  };
+
+  _.prototype._fade = function(el, type, ms) {
+    var isIn = type === 'in',
+      opacity = isIn ? 0 : 1,
+      interval = 10,
+      duration = ms,
+      gap = interval / duration,
+      self = this;
+
+    if(isIn) { // fading in
+      if(el.style.display === 'none')
+        el.style.display = '';
+      el.style.opacity = opacity;
+    }
+
+    function func() {
+      opacity = isIn ? opacity + gap : opacity - gap;
+      el.style.opacity = opacity;
+
+      if(opacity <= 0) {
+        el.style.display = 'none';
+        el.style.opacity = 1;
+      }
+
+      if(opacity <= 0 || opacity >= 1) {
+        window.clearInterval(fading);
+      }
+    }
+
+    var fading = window.setInterval(func, interval);
+  };
+
   function Kudo(element) {
     this.element = element;
     this.heartButton = element.querySelector('.kudo-button');
@@ -55,7 +114,6 @@
     kudos[this.postId] = true;
     localStorage.setItem('kudos', JSON.stringify(kudos));
     this.toggleKudo('add');
-    this.track('add');
     this.displayKudoed();
     this.displayKudoThanks();
   };
@@ -96,7 +154,6 @@
     localStorage.setItem('kudos', JSON.stringify(kudos));
     this.toggleKudo('remove');
     this.updateKudoCountDisplay(this.kudosCount);
-    this.track('remove');
     this.displayNotKudoed();
   };
 
@@ -125,18 +182,8 @@
     this.element.querySelector('.kudo-word').textContent = kudoWord;
   };
 
-  Kudo.prototype.track = function(action) {
-    ga(
-      'send',
-      'event',
-      'kudo',
-      action,
-      document.querySelector('.post-title').textContent
-    );
-  };
-
   function getKudos() {
-    if (storageSupported) {
+    if (storageSupported()) {
       var kudos = JSON.parse(localStorage.getItem('kudos'));
       if (!kudos) {
         kudos = {};
@@ -147,7 +194,7 @@
   }
 
   function getStorage(postId) {
-    if (storageSupported) {
+    if (storageSupported()) {
       return localStorage.getItem(postId);
     }
   }
