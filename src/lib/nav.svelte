2cccfb2a4ce31df0fc0f80d1a8cba98b0c763a49
Dheepak Krishnamurthy <me@kdheepak.com>
2021-12-17 02:17:23 -0700

    WIP

---
+++ src/lib/nav.svelte
@@ -0,0 +1,6 @@
+<div>
+  <nav>
+    <a href="/"><h3 class="home">HOME</h3></a>
+    <a href="/about"><h3 class="about">about</h3></a>
+  </nav>
+</div>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Header from "$lib/header/Header.svelte";
+  import "../app.css";
+  import Nav from '$lib/Nav.svelte';
+</script>
+
+<div>
+  <Header>
+  </Header>
+  <main>
+    <slot />
+  </main>
+</div>
