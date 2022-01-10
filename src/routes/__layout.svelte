43c63cf5264993a4444bd654b012133827954d08
Dheepak Krishnamurthy <me@kdheepak.com>
2022-01-10 12:05:45 -0700

    chore: Update files

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,15 @@
 <script>
+import { onMount } from 'svelte';
+
   import '../app.css'
   import '/node_modules/papercss/dist/paper.min.css'
+
+  import '../prism.css'
+  import '../prism.js'
+
+  onMount(() => Prism.highlightAll())
+
+
 </script>
 
 <main>
