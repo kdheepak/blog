f658cba5b7ae955ce049384784a15f86c290d994
Dheepak Krishnamurthy <me@kdheepak.com>
2022-01-11 12:03:24 -0700

    chore: Update files

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,12 +6,8 @@ import { onMount } from 'svelte';
   import '../css/tufte-extra.css'
   import '../css/latex.css'
   import '../css/pandoc.css'
-  import '../css/prism.css'
-
-  import '$lib/prism.js'
 
   onMount(() => {
-    Prism.highlightAll()
     let script = document.createElement('script');
     script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js";
     document.head.append(script);
