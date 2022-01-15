6301c16a27bf2bf4d8f659b69622aff96b58d540
Dheepak Krishnamurthy <me@kdheepak.com>
2022-01-15 14:16:28 -0700

    chore: Update files

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+rm -rf build
+NODE_ENV=production npm run build
+cd build
+git init
+git add -A
+git commit -m 'deploy' -n
+git push -f git@github.com:kdheepak/blog.git main:gh-pages
+cd -
