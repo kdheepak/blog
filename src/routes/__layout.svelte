7ae2a72fa2b1d157d49b5ada62c7786ffd1bed3c
Dheepak Krishnamurthy <me@kdheepak.com>
2022-01-16 06:40:37 -0700

    chore: Update files

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,12 @@
-<script>
+<script context="module">
   import '../css/tufte.css'
   import '../css/tufte-extra.css'
   import '../css/latex.css'
   import '../css/pandoc.css'
   import '../css/prism.css'
   import '../css/app.css'
+
+  import '$lib/prism'
 </script>
 
 <main class="line-numbers match-braces rainbow-braces">
