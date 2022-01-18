4a1b50888838153d5ec544443883cd52dad5d46c
Dheepak Krishnamurthy <me@kdheepak.com>
2022-01-18 01:20:10 -0700

    chore: Update files

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,3 +10,17 @@
 <main class="line-numbers match-braces rainbow-braces Prose">
   <slot />
 </main>
+
+<footer>
+  <p>
+    <a rel="license noopener noreferrer" target="_blank" href="http://creativecommons.org/licenses/by/4.0/"><img alt="This work is licensed under a Creative Commons Attribution 4.0 International License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a>
+  </p>
+</footer>
+
+<style>
+  footer {
+    width: 66vw;
+    display: flex;
+    justify-content: center;
+  }
+</style>
