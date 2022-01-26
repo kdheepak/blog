d833de1b399bd2019bf807460ec954bd8eb7d7ba
Dheepak Krishnamurthy <me@kdheepak.com>
2022-01-25 20:36:32 -0700

    chore: Update files

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -78,7 +78,6 @@
   <meta name="keywords" content={metadata.keywords}>
   <meta name="author" content="Dheepak Krishnamurthy">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <html lang="en-GB"/>
   {#if metadata.summary}
     <meta name="description" content={metadata.summary} />
   {/if}
