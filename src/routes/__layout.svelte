652ae5b180185036b7ee070a9ee5480561f3e935
Dheepak Krishnamurthy <me@kdheepak.com>
2022-03-30 13:15:26 -0600

    chore: Update files

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script context="module">
   import { base } from '$app/paths'
-  import { page } from '$app/stores';
+  import { page } from '$app/stores'
 
   import '../../static/css/app.css'
   import '../../static/css/latex.css'
@@ -13,11 +13,11 @@
 
   export async function load({ fetch }) {
     try {
-      await fetch('/rss.xml');
-      await fetch('/sitemap.xml');
-      return true;
+      await fetch('/rss.xml')
+      await fetch('/sitemap.xml')
+      return true
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
   }
 </script>
@@ -27,7 +27,7 @@
     if (!url.startsWith('/tags')) {
       return false
     }
-    const chunks = url.split('/').filter(c => c != '')
+    const chunks = url.split('/').filter((c) => c != '')
     if (chunks[chunks.length - 1] == 'tags' || chunks.length == 1) {
       return false
     }
@@ -42,15 +42,24 @@
 
 <footer>
   <p>
-    <a rel="license noopener noreferrer" target="_blank" href="https://creativecommons.org/licenses/by/4.0/"><img alt="This work is licensed under a Creative Commons Attribution 4.0 International License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a>
+    <a
+      rel="license noopener noreferrer"
+      target="_blank"
+      href="https://creativecommons.org/licenses/by/4.0/"
+      ><img
+        alt="This work is licensed under a Creative Commons Attribution 4.0 International License"
+        style="border-width:0"
+        src="https://i.creativecommons.org/l/by/4.0/88x31.png"
+      /></a
+    >
   </p>
-  <br/>
+  <br />
   <div class="icons">
     <div class="icon">
-      <a href="https://github.com/kdheepak" target="_blank"><DiGithubBadge/></a>
+      <a href="https://github.com/kdheepak" target="_blank"><DiGithubBadge /></a>
     </div>
     <div class="icon">
-      <a rel="external" href="{rssFeed}"><FaRssSquare/></a>
+      <a rel="external" href={rssFeed}><FaRssSquare /></a>
     </div>
   </div>
 </footer>
