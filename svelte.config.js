145194d5ea4c472c5fcdfead3ed1e771fbe7179a
Dheepak Krishnamurthy <me@kdheepak.com>
2022-03-30 23:29:07 -0600

    chore: Update files ♻️

--- svelte.config.js
+++ svelte.config.js
@@ -354,15 +354,6 @@ function debugPreprocess(message) {
   }
 }
 
-function getPages() {
-  let pages = ['*']
-  const { slugs } = fromDir('src/posts/', '.md')
-  for (const p of slugs) {
-    pages.push(`/${p}`)
-  }
-  return pages
-}
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', '.md'],
@@ -379,7 +370,6 @@ const config = {
     prerender: {
       concurrency: 4,
       crawl: true,
-      entries: getPages(),
     },
   },
 }
