43ac1b1a91d4cc4930af3370f9f45f91689e3542
Dheepak Krishnamurthy <me@kdheepak.com>
2022-07-27 01:32:41 -0600

    chore: Update files ♻️

--- pandoc/fix-image-links.lua
+++ pandoc/fix-image-links.lua
@@ -3,19 +3,19 @@ local stringify = (require("pandoc.utils")).stringify
 local utils = require("pandoc.utils")
 
 function startswith(text, prefix)
-	return text:find(prefix, 1, true) == 1
+  return text:find(prefix, 1, true) == 1
 end
 
 function Image(elem)
-	if startswith(elem.src, "images/") then
-		elem.src = "./" .. elem.src
-	end
-	if startswith(elem.src, "videos/") then
-		elem.src = "./" .. elem.src
-	end
-	return elem
+  if startswith(elem.src, "images/") then
+    elem.src = "./" .. elem.src
+  end
+  if startswith(elem.src, "videos/") then
+    elem.src = "./" .. elem.src
+  end
+  return elem
 end
 
 return {
-	{ Image = Image },
+  { Image = Image },
 }
