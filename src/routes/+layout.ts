7f54287689c67e496c9e1c82e5dc0989bf6077f5
Dheepak Krishnamurthy <me@kdheepak.com>
2022-08-22 11:09:25 -0600

    chore: Update files ♻️

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,13 +1,5 @@
-import "../../static/css/app.css";
-import "../../static/css/latex.css";
-import "../../static/css/pandoc.css";
-import "../../static/css/tufte-extra.css";
-import "../../static/css/tufte.css";
-import "../../static/css/stork.css";
-
 export async function load({ fetch }) {
   try {
-    await fetch("/rss.xml");
     await fetch("/sitemap.xml");
     return;
   } catch (error) {
