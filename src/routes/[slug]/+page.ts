f3a12cd0219f4508e0aba6ab54a518bfb00d3f21
Dheepak Krishnamurthy <me@kdheepak.com>
2022-08-22 19:49:53 -0600

    chore: Update files ♻️

--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -4,8 +4,8 @@ import { base } from "$app/paths";
 
 const allPosts = import.meta.glob("/src/posts/*.md");
 
-export const load: PageLoad = async ({ params, parent, data }) => {
+export async function load({ params, parent, data }) {
   const { metadata } = data;
   const component = (await allPosts[`/${metadata.path}`]()).default;
   return { component, metadata };
-};
+}
