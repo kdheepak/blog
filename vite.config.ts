3fb60ac58804743a7f55cb438bb3e993c2b747b6
Dheepak Krishnamurthy <me@kdheepak.com>
2023-02-06 22:37:16 -0500

    fix: Update formatting 🐛

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { searchForWorkspaceRoot, defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { searchForWorkspaceRoot, defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		fs: {
-			allow: [
-				// search up for workspace root
-				searchForWorkspaceRoot(process.cwd())
-			]
-		}
-	}
+  plugins: [sveltekit()],
+  server: {
+    fs: {
+      allow: [
+        // search up for workspace root
+        searchForWorkspaceRoot(process.cwd()),
+      ],
+    },
+  },
 });
