98a0716c58f5b7b04020878f55e63b3fec021f0b
Dheepak Krishnamurthy <me@kdheepak.com>
2023-02-27 08:18:20 -0500

    Update +page.server.ts

--- src/routes/tags/+page.server.ts
+++ src/routes/tags/+page.server.ts
@@ -1,7 +1,7 @@
 import { getPostsMetadata } from "$lib/posts";
 
 export function load() {
-  const { posts, tags } = getPostsMetadata("src/posts/", ".md");
+  const { posts, tags } = getPostsMetadata("src/posts/", ".md", true);
   const options = { year: "numeric", month: "short", day: "numeric", weekday: "short" };
   const humanDate = new Date().toLocaleDateString(undefined, options);
   return { tags, humanDate, posts };
