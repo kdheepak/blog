b35a2f086b0a7bf2aa93fd09d17ab4ef6e5106f5
Dheepak Krishnamurthy <me@kdheepak.com>
2023-03-12 20:25:41 -0400

    chore: Update files ♻️

--- _quarto/post-render.jl
+++ _quarto/post-render.jl
@@ -1,4 +1,10 @@
+# Setup RSS
+
 const srcFilePath = "./_site/index-julia.xml"
 const dstFilePath = "./_site/tags/julia/rss.xml"
 mkpath(dirname(dstFilePath))
 cp(srcFilePath, dstFilePath, force=true)
+
+# Setup downloads
+
+cp("./_quarto/downloads", "./_site/downloads", force=true)
