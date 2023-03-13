3bd3c31ea09eef7f5e5bb8517b53a9c6efe02d69
Dheepak Krishnamurthy <me@kdheepak.com>
2023-03-12 20:49:28 -0400

    chore: Update files ♻️

--- _quarto/post-render.jl
+++ _quarto/post-render.jl
@@ -12,3 +12,12 @@ cp("./_quarto/downloads", "./_site/downloads", force=true)
 # Setup CNAME
 
 cp("./_quarto/CNAME", "./_site/CNAME", force=true)
+
+# RSS style
+
+cp("./_quarto/index.xsl", "./_site/index.xsl", force=true)
+lines = readlines("./_site/index.xml")
+insert!(lines, 2, raw"""<?xml-stylesheet type="text/xsl" media="screen" href="/index.xsl"?>""")
+open("./_site/index.xml", "w") do f
+  write(f, join(lines, "\n"))
+end
